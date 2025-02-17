692a32e97980aeed98c6635769f6b3a382e94c41
Akash Tolange <akashhtolangee77@gmail.com>
2025-02-17 20:03:16 +0545

    Renamed App.js to App.jsx, updated imports, and fixed JSX syntax

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Your styles
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
 
